8b46f96b9a4b605e7b92c32c346479806f57b6da
Logan Pageler <pagelerlogan@gmail.com>
2023-03-31 02:03:19 -0700

    Made Compass Api

---
+++ Path-Finding/src/Main.java
@@ -0,0 +1,26 @@
+import compass.App;
+import maze.Maze;
+import maze.generators.MazeGenerator;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Maze maze = new Maze(5, 5);
+        System.out.println(maze);
+
+        MazeGenerator gen = new MazeGenerator(5, 5);
+        System.out.println(gen.generateMaze());
+
+        gen = gen.setStart(0, 0).setEnd(4, 4);
+        System.out.println(gen.generateMaze());
+
+        App app = new App(80);
+
+        app.addRoute("/maze", (req, res) -> {
+            MazeGenerator gen2 = new MazeGenerator(5, 5);
+            res.setBody("<html>" + gen2.generateMaze().toString().replace("\n", "<br>") + "</html>");
+        });
+
+        app.start();
+    }
+}
