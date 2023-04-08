8895b4eb70536fde920840c7f1c8d49298e88a4a
Logan Pageler <pagelerlogan@gmail.com>
2023-04-07 22:00:30 -0700

    fixed workspace

--- sort-wiki/src/Main.java
+++ sort-wiki/src/Main.java
@@ -1,6 +1,5 @@
 import compass.App;
 import compass.http.Session;
-import maze.generators.MazeGenerator;
 
 public class Main {
 
@@ -13,11 +12,9 @@ public class Main {
 
         app.getDefaultMessage().addHeader("Access-Control-Allow-Origin", "*");
 
-        app.addRoute("/maze", (req, res, ses) -> {
+        app.addRoute("/", (req, res, ses) -> {
             System.out.println(req);
-            MazeGenerator gen2 = new MazeGenerator(5, 5);
-            res.setBody("<html>" + gen2.generateMaze().toString().replace("\n", "<br>") +
-                    "</html>");
+            res.setBody("Hello Word!");
         });
 
         app.addRoute("/ed", (req, res, ses) -> {
--- sort-wiki/src/solvers/Solver.java
+++
@@ -1,5 +0,0 @@
-package solvers;
-
-public abstract class Solver {
-
-}
