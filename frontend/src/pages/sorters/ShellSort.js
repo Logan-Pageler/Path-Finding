f4a14953ffcbad2c658d3fdeb8d0016838ce5690
Evan Ohme <evanohme@arizona.edu>
2023-04-11 00:30:52 -0700

    small update to Shellsort desc

--- frontend/src/pages/sorters/ShellSort.js
+++ frontend/src/pages/sorters/ShellSort.js
@@ -12,15 +12,15 @@ function ShellSort() {
             to the number that is greater than or equal to the length of the 
             array divided by three. The algorithm performs insertion sort on every 
             other h-indexed value starting at index 0. The h-value then updates to 
-            3 * (the previous h value) + 1 and repeats the insertion sort. The h-value 
+            3 * (the previous h value) + 1 and repeats the insertion sort. The h value 
             updates until it reaches one, once h equals one insertion sort is performed 
             on the array. Shell sort requires small amounts of code, good for moderately 
             large arrays, and space usage is O(1). Shell sort is not stable, meaning the 
-            order of the array is not preserved. It is as well sensitive to input, 
-            meaning the array values will affect the runtime/work the algorithm does. 
-            Although its performance depends on the number of increments of h, and the 
-            size of the common divisor the best-case runtime is O(NlogN), and the 
-            worst-case along with the average-case runtime is O(N^(3/2)).
+            order of the array is not preserved. It is sensitive to input, meaning the 
+            array values will affect the runtime of the algorithm. Its performance 
+            depends on the number of increments of h and the size of the common divisor. 
+            The best-case runtime is O(NlogN) which occurs when the array is already sorted. 
+            The worst-case along with the average-case runtime is O(N^(3/2)). 
             </p>
         </>
     );
